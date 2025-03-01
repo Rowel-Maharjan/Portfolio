41810c147974cf6dc5e590cb6356ac67404b9350
Rowel Maharjan <rowelmhj@gmail.com>
2025-03-01 12:42:19 +0545

    Loader

--- src/components/CanvasLoader.jsx
+++ src/components/CanvasLoader.jsx
@@ -1,11 +1,13 @@
 import { Html, useProgress } from "@react-three/drei"
-
+import './loader.css'
 const CanvasLoader = () => {
     const { progress } = useProgress()
     return (
         <Html as="div" style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }} center>
             <p style={{ fontSize: 14, color: "#f1f1f1", fontWeight: 400, marginTop: 40 }}>
-                {progress !== 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
+               {
+                progress && <div className="loader"></div>
+               }
             </p>
 
         </Html>
