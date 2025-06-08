ecb0ea1c943e857869d5452c9534c332442ce9e5
Rowel Maharjan <rowelmhj@gmail.com>
2025-06-08 19:51:39 +0545

    Logos Add

--- src/assets/index.js
+++ src/assets/index.js
@@ -12,30 +12,30 @@ import threejs from "./tech/threejs.svg";
 import express from "./tech/express.png";
 import next from "./tech/Next.png";
 
-
 import makemytrip from "./makemytrip.png";
 import passwordmanager from "./passwordmanager.png";
 import spotify from "./spotify.png";
 import todo from "./todo.png";
 import productdetail from "./productdetail.png";
 import twitter from "./twitter.png";
-import logo from './logo.png';
-import twittersvg from './twitter.svg'
-import insta from './instagram.svg'
-import github from './github.svg'
-import grid1 from './grid1.png'
-import grid2 from './grid2.png'
-import grid3 from './grid3.png'
-import grid4 from './grid4.png'
-import arrowup from './arrow-up.png'
-import tick from './tick.svg'
-import copy from './copy.svg'
-import jobPortal from './JobPortal.png'
-import netflix from './netflix.png'
-import swasthalaya from './swasthalayas.png'
+import logo from "./logo.png";
+import twittersvg from "./twitter.svg";
+import insta from "./instagram.svg";
+import github from "./github.svg";
+import grid1 from "./grid1.png";
+import grid2 from "./grid2.png";
+import grid3 from "./grid3.png";
+import grid4 from "./grid4.png";
+import arrowup from "./arrow-up.png";
+import tick from "./tick.svg";
+import copy from "./copy.svg";
+import jobPortal from "./JobPortal.png";
+import netflix from "./netflix.png";
+import swasthalaya from "./swasthalayas.png";
+import nexbilLogo from "./nexbil-logo.webp";
+import bluefoxLogo from "./bluefox-logo.jpeg";
 
-import CV from './CV - Rowel Maharjan.pdf'
-
+import CV from "./CV - Rowel Maharjan.pdf";
 
 export {
   css,
@@ -71,5 +71,7 @@ export {
   CV,
   jobPortal,
   netflix,
-  swasthalaya
+  swasthalaya,
+  nexbilLogo,
+  bluefoxLogo
 };
--- src/config.js
+++ src/config.js
@@ -21,6 +21,8 @@ import {
   jobPortal,
   netflix,
   swasthalaya,
+  nexbilLogo,
+  bluefoxLogo,
 } from "./assets/index.js";
 
 export const navLinks = [
@@ -480,14 +482,13 @@ export const bottomPart = [
   },
 ];
 
-
 export const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    // icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    title: "Web Developer (Internship)",
+    company_name: "Blue Fox",
+    icon: bluefoxLogo,
+    iconBg: "#FFFFFF",
+    date: "November 2024 - December 2024",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -496,37 +497,25 @@ export const experiences = [
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    // icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    title: "Web Developer (Internship)",
+    company_name: "Nexbil",
+    icon: nexbilLogo,
+    iconBg: "#FFFFFF",
+    date: "January 2025 - March 2025",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Developing and maintaining web applications using React.js, Next.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    // icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
+
   {
     title: "Full stack Developer",
-    company_name: "Meta",
-    // icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    company_name: "Nexbil",
+    icon: nexbilLogo,
+    iconBg: "#FFFFFF",
+    date: "March 2025 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
